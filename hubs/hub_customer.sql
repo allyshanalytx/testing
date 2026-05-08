423d859d9e4b2bd6c4b399410d7f404d3f0444bf
allyshanalytx <allysha.abdullah@analytx.com.my>
2026-05-08 17:29:05 +0800

    Update hub_customer.sql

--- hubs/hub_customer.sql
+++ hubs/hub_customer.sql
@@ -1,1 +1,10 @@
+-- Hub: Customer
+-- Stores unique business keys for customers
 
+CREATE TABLE hub_customer (
+    customer_hk      CHAR(32)      NOT NULL,  -- hash key
+    customer_id      VARCHAR(50)   NOT NULL,  -- business key
+    load_date        TIMESTAMP     NOT NULL,
+    record_source    VARCHAR(100)  NOT NULL,
+    CONSTRAINT pk_hub_customer PRIMARY KEY (customer_hk)
+);
